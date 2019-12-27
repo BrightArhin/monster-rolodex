a6c1707e764ff7bdcc333a1ff30097e0c477a1be
Bright <barhin49@gmail.com>
2019-12-27 09:45:44 +0000

    Updates

---
+++ precache-manifest.dcb4fcef9c790c889a1cd4e078d05285.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8729c3e8eeeb10c4f339d4337394b072",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "1a912edae81e17a76124",
+    "url": "/monster-rolodex/static/css/main.71326572.chunk.css"
+  },
+  {
+    "revision": "936b7beb1d9340026d92",
+    "url": "/monster-rolodex/static/js/2.ce689812.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.ce689812.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1a912edae81e17a76124",
+    "url": "/monster-rolodex/static/js/main.edab9e2f.chunk.js"
+  },
+  {
+    "revision": "6cceb7ac6caa148b2b07",
+    "url": "/monster-rolodex/static/js/runtime-main.779b607f.js"
+  }
+]);
